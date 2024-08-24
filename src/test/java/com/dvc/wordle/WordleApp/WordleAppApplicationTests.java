d2062a89cab89d4bb46944d5b805e4057710841f
0xDVC <neilohene@gmail.com>
2024-08-24 09:57:19 +0000

    init: setting up project

---
+++ src/test/java/com/dvc/wordle/WordleApp/WordleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvc.wordle.WordleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
